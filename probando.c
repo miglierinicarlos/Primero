c9012dbcd798dd89284b4a2f20d9e6eaeb52b8f1
miglierinicarlos <miglierinicarlos@gmail.com>
2018-08-31 00:52:21 -0300

    probar

---
+++ probando.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+long factorial(int n);
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n Hola a todos");
+	printf("%s\n Mi segundo commit");
+	printf("%s\n 1234" );
+	printf("%s\n Ha");
+	return 0;
+}
+long factorial(int n){
+	if(n<=1){
+		return 1;
+	}else{
+		return (n*factorial(n-1));
+	}
+}
