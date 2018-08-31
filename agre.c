0501c8f800e32dd4f0dc0951b0347ad4a68ef6eb
miglierinicarlos <miglierinicarlos@gmail.com>
2018-08-31 00:18:42 -0300

    agrego numeros

--- agre.c
+++ agre.c
@@ -4,6 +4,6 @@ int main(int argc, char const *argv[])
 {
 	printf("%s\n Hola a todos");
 	printf("%s\n Mi segundo commit");
-	printf("%s\n  Sigo practicando" );
+	printf("%s\n 1234" );
 	return 0;
 }
