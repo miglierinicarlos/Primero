f55b996293be9f917ca5ad98532ec00e3c8bc4ea
miglierinicarlos <miglierinicarlos@gmail.com>
2018-08-31 14:18:43 -0300

    agregando func suma

--- f.c
+++ f.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 void saludo();
+int sumar(int n1, int n2);
 
 int main(int argc, char const *argv[])
-{
+{ 
+	int a,b,suma=0;
+
+	printf("\nDigite 2 numeros\n");
+	scanf("%i %i", &a, &b);
+
+	suma = sumar(a,b);
+
+	printf("\nLa suma es: %i", suma);
+
 	saludo();
 
 	return 0;
 }
 void saludo(){
 	printf("\nHola que tal\n");
+}
+int sumar(int n1, int n2){
+	int suma = 0;
+	suma = n1+n2;
+
+	return suma;
 }
